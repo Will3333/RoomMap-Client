e9343431fe60e1876ccc95df6b840c8024dfbcb3
William Smith <william.smith@wsmi.pro>
2020-10-05 13:46:40 +0200

    - Adding WR filter into Freemarker template, CSS and js files

--- resources/js/roommap-client-js-matrix-rooms-page-native.js
+++ resources/js/roommap-client-js-matrix-rooms-page-native.js
@@ -26,9 +26,17 @@ function showGAFilterBlock() {
 function hideGAFilterBlock() {
 	gaFilterBlock.style.display = "none";
 }
+function showWRFilterBlock() {
+	wrFilterBlock.style.display = "block";
+}
+function hideWRFilterBlock() {
+	wrFilterBlock.style.display = "none";
+}
 
 const gaFilterDisplayButton = document.getElementById("matrix-rooms-ga-filter-display-button");
+const wrFilterDisplayButton = document.getElementById("matrix-rooms-wr-filter-display-button");
 const gaFilterBlock = document.getElementById("matrix-rooms-ga-header-filter-block");
+const wrFilterBlock = document.getElementById("matrix-rooms-wr-header-filter-block");
 
 const urlParams = new URLSearchParams(window.location.search);
 const gaFilterUrlParam = urlParams.get(MATRIX_ROOMS_PAGE_GA_FILTER_REQ_NAME);
@@ -41,6 +49,14 @@ gaFilterDisplayButton.addEventListener('click', (event) => {
 	else
 		hideGAFilterBlock();
 });
+wrFilterDisplayButton.addEventListener('click', (event) => {
+	if (!wrFilterBlock.style.display || wrFilterBlock.style.display == "none")
+		showWRFilterBlock();
+	else
+		hideWRFilterBlock();
+});
 
 if (gaFilterUrlParam != null && gaFilterUrlParam != 'NO_FILTER')
 	showGAFilterBlock();
+if (wrFilterUrlParam != null && wrFilterUrlParam != 'NO_FILTER')
+	showWRFilterBlock();
