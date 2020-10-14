f3f41c5ab11fa8ecf5c3e1b79417b1cf2dbb30bf
William Smith <william.smith@wsmi.pro>
2020-10-14 17:25:17 +0200

    Apache License

--- roommap-client-js-matrix-rooms-page/build.gradle.kts
+++ roommap-client-js-matrix-rooms-page/build.gradle.kts
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2020 William Smith
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the License.
+ */
+
 plugins {
     id("org.jetbrains.kotlin.js") version "1.4.10"
     kotlin("plugin.serialization") version "1.4.10"
@@ -21,8 +31,6 @@ kotlin {
         implementation(project(":roommap-client-lib"))
         /*
         implementation("pro.wsmi:roommap-lib:${rootProject.extra["ROOMMAP_LIB_VERSION"]}")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${rootProject.extra["COROUTINES_VERSION"]}")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:${rootProject.extra["COROUTINES_VERSION"]}")
         implementation("pro.wsmi:kwsmilib:${rootProject.extra["KWSMILIB_VERSION"]}")
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:${rootProject.extra["SERIALIZATION_VERSION"]}")
         implementation("io.ktor:ktor-client-core:${rootProject.extra["KTOR_VERSION"]}")
