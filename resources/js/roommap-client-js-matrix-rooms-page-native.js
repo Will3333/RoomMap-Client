c68b135e6e3fd4a214059ab306f1d38cb48528e9
William Smith <william.smith@wsmi.pro>
2020-10-17 16:59:18 +0200

    Little improving of code

--- resources/js/roommap-client-js-matrix-rooms-page-native.js
+++ resources/js/roommap-client-js-matrix-rooms-page-native.js
@@ -23,20 +23,20 @@ const gaTooltipBlock = document.getElementById(GA_TOOLTIP_ID);
 const wrTooltipContainer = document.getElementById(WR_TOOLTIP_CONTAINER_ID);
 const wrTooltipBlock = document.getElementById(WR_TOOLTIP_ID);
 
-gaTooltipContainer.addEventListener('mouseenter', e => {
+gaTooltipContainer.addEventListener('mouseenter', () => {
     if (gaTooltipBlock != null && gaTooltipBlock.classList.contains(TOOLTIP_BLOCK_HIDDEN_CLASS))
         gaTooltipBlock.classList.replace(TOOLTIP_BLOCK_HIDDEN_CLASS, TOOLTIP_BLOCK_DISPLAYED_CLASS);
 });
-gaTooltipContainer.addEventListener('mouseleave', e => {
+gaTooltipContainer.addEventListener('mouseleave', () => {
     if (gaTooltipBlock != null && gaTooltipBlock.classList.contains(TOOLTIP_BLOCK_DISPLAYED_CLASS))
         gaTooltipBlock.classList.replace(TOOLTIP_BLOCK_DISPLAYED_CLASS, TOOLTIP_BLOCK_HIDDEN_CLASS);
 });
 
-wrTooltipContainer.addEventListener('mouseenter', e => {
+wrTooltipContainer.addEventListener('mouseenter', () => {
     if (wrTooltipBlock != null && wrTooltipBlock.classList.contains(TOOLTIP_BLOCK_HIDDEN_CLASS))
         wrTooltipBlock.classList.replace(TOOLTIP_BLOCK_HIDDEN_CLASS, TOOLTIP_BLOCK_DISPLAYED_CLASS);
 });
-wrTooltipContainer.addEventListener('mouseleave', e => {
+wrTooltipContainer.addEventListener('mouseleave', () => {
     if (wrTooltipBlock != null && wrTooltipBlock.classList.contains(TOOLTIP_BLOCK_DISPLAYED_CLASS))
         wrTooltipBlock.classList.replace(TOOLTIP_BLOCK_DISPLAYED_CLASS, TOOLTIP_BLOCK_HIDDEN_CLASS);
 });
